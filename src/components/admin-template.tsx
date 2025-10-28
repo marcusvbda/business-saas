74d5d3789690e2c017210e2661d1669f980db0ac
Vinicius Bassalobre <bassalobre.vinicius@gmail.com>
2025-10-28 19:22:45 +0000

    corrige list

--- src/components/admin-template.tsx
+++ src/components/admin-template.tsx
@@ -19,7 +19,6 @@ import { Fragment } from 'react/jsx-runtime';
 import Link from 'next/link';
 import { useGlobalStore } from '@/stores/global';
 import Loading from './fallback';
-import ProtectedPage from './protected-page';
 
 interface IProps extends IWithChild {
 	breadcrumb?: IBreadcrumbItem[];
@@ -29,51 +28,49 @@ export default function AdminTemplate({ children, breadcrumb = [] }: IProps) {
 	const { globalLoading } = useGlobalStore();
 
 	return (
-		<ProtectedPage>
-			<SidebarProvider>
-				<AppSidebar />
-				<SidebarInset>
-					<header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12 w-full">
-						<div className="flex items-center gap-2 px-4 w-full">
-							<div className="md:hidden lg:hidden xl:hidden flex items-center gap-2">
-								<SidebarTrigger className="-ml-1 block" />
-								<Separator
-									orientation="vertical"
-									className="mr-2 data-[orientation=vertical]:h-4"
-								/>
-							</div>
-							<Breadcrumb>
-								<BreadcrumbList>
-									{breadcrumb.map((x: IBreadcrumbItem, index: any) => (
-										<Fragment key={index}>
-											<BreadcrumbItem className="hidden md:block">
-												{x?.href ? (
-													<Link href={x.href}>
-														<BreadcrumbPage className="opacity-50">
-															{x.label}
-														</BreadcrumbPage>
-													</Link>
-												) : (
-													<BreadcrumbPage>{x.label}</BreadcrumbPage>
-												)}
-											</BreadcrumbItem>
-											{index !== breadcrumb.length - 1 && (
-												<BreadcrumbSeparator className="hidden md:block" />
-											)}
-										</Fragment>
-									))}
-								</BreadcrumbList>
-							</Breadcrumb>
-							<div className="ml-auto">
-								<ThemeSwitcher />
-							</div>
+		<SidebarProvider>
+			<AppSidebar />
+			<SidebarInset>
+				<header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12 w-full">
+					<div className="flex items-center gap-2 px-4 w-full">
+						<div className="md:hidden lg:hidden xl:hidden flex items-center gap-2">
+							<SidebarTrigger className="-ml-1 block" />
+							<Separator
+								orientation="vertical"
+								className="mr-2 data-[orientation=vertical]:h-4"
+							/>
 						</div>
-					</header>
-					<div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-						{globalLoading ? <Loading /> : children}
+						<Breadcrumb>
+							<BreadcrumbList>
+								{breadcrumb.map((x: IBreadcrumbItem, index: any) => (
+									<Fragment key={index}>
+										<BreadcrumbItem className="hidden md:block">
+											{x?.href ? (
+												<Link href={x.href}>
+													<BreadcrumbPage className="opacity-50">
+														{x.label}
+													</BreadcrumbPage>
+												</Link>
+											) : (
+												<BreadcrumbPage>{x.label}</BreadcrumbPage>
+											)}
+										</BreadcrumbItem>
+										{index !== breadcrumb.length - 1 && (
+											<BreadcrumbSeparator className="hidden md:block" />
+										)}
+									</Fragment>
+								))}
+							</BreadcrumbList>
+						</Breadcrumb>
+						<div className="ml-auto">
+							<ThemeSwitcher />
+						</div>
 					</div>
-				</SidebarInset>
-			</SidebarProvider>
-		</ProtectedPage>
+				</header>
+				<div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+					{globalLoading ? <Loading /> : children}
+				</div>
+			</SidebarInset>
+		</SidebarProvider>
 	);
 }
